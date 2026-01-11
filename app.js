623b2503763e86fdd7ba4b5ec2994bb09fba4a75
krushnaShewale <krushnashewale8008@gmail.com>
2026-01-11 11:58:37 +0530

    add error handlers

--- app.js
+++ app.js
@@ -6,22 +6,26 @@ const path = require("path");
 const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
 
+const wrapAsync = require("./utilities/wrapAsync.js");
+const ExpressError = require("./utilities/expressError.js");
+
 const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
 
 // DB connection
 main()
-  .then(() => console.log("connected to DB"))
+  .then(() => console.log("Connected to DB"))
   .catch(err => console.log(err));
 
 async function main() {
   await mongoose.connect(MONGO_URL);
 }
 
-// enable ejs-mate
+// View engine
 app.engine("ejs", ejsMate);
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
+// Middleware
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
 app.use(express.static(path.join(__dirname, "public")));
@@ -32,10 +36,10 @@ app.get("/", (req, res) => {
 });
 
 // INDEX
-app.get("/listings", async (req, res) => {
+app.get("/listings", wrapAsync(async (req, res) => {
   const allListings = await Listing.find({});
   res.render("listings/index.ejs", { allListings });
-});
+}));
 
 // NEW
 app.get("/listings/new", (req, res) => {
@@ -43,36 +47,57 @@ app.get("/listings/new", (req, res) => {
 });
 
 // CREATE
-app.post("/listings", async (req, res) => {
+app.post("/listings", wrapAsync(async (req, res) => {
   const newListing = new Listing(req.body.listing);
   await newListing.save();
   res.redirect("/listings");
-});
+}));
 
 // SHOW
-app.get("/listings/:id", async (req, res) => {
+app.get("/listings/:id", wrapAsync(async (req, res) => {
   const listing = await Listing.findById(req.params.id);
+  if (!listing) {
+    throw new ExpressError(404, "Listing not found");
+  }
   res.render("listings/show.ejs", { listing });
-});
+}));
 
 // EDIT
-app.get("/listings/:id/edit", async (req, res) => {
+app.get("/listings/:id/edit", wrapAsync(async (req, res) => {
   const listing = await Listing.findById(req.params.id);
+  if (!listing) {
+    throw new ExpressError(404, "Listing not found");
+  }
   res.render("listings/edit.ejs", { listing });
-});
+}));
 
 // UPDATE
-app.put("/listings/:id", async (req, res) => {
+app.put("/listings/:id", wrapAsync(async (req, res) => {
   await Listing.findByIdAndUpdate(req.params.id, req.body.listing);
   res.redirect(`/listings/${req.params.id}`);
-});
+}));
 
 // DELETE
-app.delete("/listings/:id", async (req, res) => {
+app.delete("/listings/:id", wrapAsync(async (req, res) => {
   await Listing.findByIdAndDelete(req.params.id);
   res.redirect("/listings");
+}));
+
+// 404
+app.use((req, res, next) => {
+  
+  next(new ExpressError(404, "Page not found"));
+  
 });
 
+// ERROR HANDLER
+app.use((err, req, res, next) => {
+  const { statusCode = 500, message = "Something went wrong" } = err;
+  res.render("listings/error.ejs", { message });
+  
+});
+
+// SERVER
 app.listen(3000, () => {
   console.log("Server running on port 3000");
 });
